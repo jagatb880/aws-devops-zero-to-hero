7e2f325b3f8a1c9593713b2c927077aac4da3e25
Jagat Bandhu Sahoo <jagatb880@gmail.com>
2024-01-19 11:56:31 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,5 +3,6 @@ set -e
 
 # Stop the running container (if any)
 docker stop $(docker ps -q)
+
 # Delete the stop container (if any)
 docker images | grep "simple" | awk '{print $3}' | xargs -r docker rmi -f
